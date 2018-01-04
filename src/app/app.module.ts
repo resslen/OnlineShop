6911761be2b1d71c00d7a2de7d42fd9ab4b73f8b
resslen <robbert159@wp.pl>
2018-01-04 19:19:11 +0100

    tabelka oraz komponenty

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,18 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import {ProductListComponent} from './products/products-list.component';
+import { UserLoginRegistrationComponent } from './user/user-login-registration.component';
+import { SettingsComponent } from './user/settings.component';
+import { ProductDetailComponent } from './products/product-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    UserLoginRegistrationComponent,
+    SettingsComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule
