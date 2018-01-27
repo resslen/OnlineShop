b4a60af3b74344c60484b9081debf951a2d1ac35
resslen <robbert159@wp.pl>
2018-01-27 20:52:36 +0100

    utworzenie serwisu dla listy produktów

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import {ProductService} from './products/product.service';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+
+    providers: [ ProductService ]
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
 }
--- src/app/products/products-list.component.ts
+++ src/app/products/products-list.component.ts
@@ -1,30 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {IProduct} from './product';
+import {ProductService} from './product.service';
 
 @Component({
     templateUrl: './products-list.component.html'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
     pageTitle: 'Product List';
-    products: any[] =  [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2016",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-        }
-    ];
+    _listFilter: string;
+    get listFilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value) {
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFiltering(this.listFilter) : this.products;
+    }
+    filteredProducts: IProduct[];
+    products: IProduct[] =  [];
+
+    constructor(private _productService: ProductService) {
+        this._listFilter = '';
+    }
+
+    performFiltering(filterBy): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) => product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+
+    ngOnInit(): void {
+        this.products = this._productService.getProducts();
+        this.filteredProducts = this.products;
+    }
 }
